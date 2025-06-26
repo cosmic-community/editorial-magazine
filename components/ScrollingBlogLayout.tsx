7898c23bfb3e68be3799930d63aaf3f09b146107
Cosmic AI <support@cosmicjs.com>
2025-06-25 18:15:28 -0700

    Fix TypeScript error by adding missing totalArticles prop to BlogSlide component

--- components/ScrollingBlogLayout.tsx
+++ components/ScrollingBlogLayout.tsx
@@ -87,6 +87,7 @@ export default function ScrollingBlogLayout({ articles }: ScrollingBlogLayoutPro
               currentSlide={currentSlide}
               isActive={index === currentSlide}
               isScrolling={isScrolling}
+              totalArticles={articles.length}
             />
           ))}
         </div>
